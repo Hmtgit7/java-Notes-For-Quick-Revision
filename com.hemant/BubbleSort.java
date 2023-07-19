908cb86374f68998e6c7fe5debc34c5446f48df8
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-07-19 20:01:33 +0530

    modify

--- com.hemant/BubbleSort.java
+++ com.hemant/BubbleSort.java
@@ -8,15 +8,12 @@ public class BubbleSort {
     }
 
     static void bubbleSort(int[] nums) {
-        boolean swapped;
         for (int i = 0; i < nums.length; i++) {
-            swapped=false;
             for (int j = 1; j < nums.length - i; j++) {
                 if (nums[j - 1] > nums[j]) {
                     int temp = nums[j - 1];
                     nums[j - 1] = nums[j];
                     nums[j] = temp;
-                    swapped=true;
                 }
             }
         }
