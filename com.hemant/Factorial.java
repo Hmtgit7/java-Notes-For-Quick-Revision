c7cb5962a2474bb3c15dd2fab2667ee3c2635f94
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-07-20 10:31:11 +0530

    factorial using recursion

--- com.hemant/Factorial.java
+++ com.hemant/Factorial.java
@@ -1,3 +1,12 @@
 public class Factorial {
-    
+    public static void main(String[] args) {
+        int n=0;
+        int result=factorialOfNumber(n);   
+        System.out.println(result);
+    }
+    public static int factorialOfNumber(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return n*factorialOfNumber(n-1);
+    }
 }
