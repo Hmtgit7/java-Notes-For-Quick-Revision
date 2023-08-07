d3bd10a100de25e27f63d6b043e3b898a6fab118
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-08-07 23:35:36 +0530

    linkedlist

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,74 @@
+package LinkedList;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LinkedList() {
+        this.size = 0;
+    }
+
+    public void insertFirst(int value) {
+        Node newNode = new Node(value);
+        newNode.next = head;
+        head = newNode;
+        if (tail == null) {
+            tail = head;
+        }
+        size++;
+    }
+
+    public void insertLast(int value) {
+        Node newNode = new Node(value);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void insertAtIndex(int value, int index) {
+        if (index == 0) {
+            insertFirst(value);
+        }
+        if (index == size) {
+            insertLast(value);
+        } else {
+            Node currentNode = head;
+            for (int i = 0; i < index - 1; i++) {
+                currentNode = currentNode.next;
+            }
+            Node newNode = new Node(value, currentNode.next);
+            currentNode.next = newNode;
+            size++;
+        }
+    }
+
+    public void display() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.print(currentNode.value + "-->");
+            currentNode = currentNode.next;
+        }
+        System.out.println("END");
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+
+        Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+}
---
+++ LinkedList/Main.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args){
+        LinkedList list=new LinkedList();
+        list.insertFirst(10);
+        list.insertFirst(20);
+        list.insertFirst(30);
+        list.insertFirst(40);
+        list.insertFirst(50);
+
+        list.insertLast(60);
+        list.insertLast(70);
+
+        list.insertAtIndex(80,3);
+        list.insertAtIndex(90,4);
+        list.display();
+    }
+
+}
