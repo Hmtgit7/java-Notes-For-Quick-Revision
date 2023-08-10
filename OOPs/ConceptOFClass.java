4bb140ffee9f84039f0897c33fc214bc4b0d77f9
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-08-10 22:55:38 +0530

    Basic concept of object oriented programming

--- OOPs/ConceptOFClass.java
+++ OOPs/ConceptOFClass.java
@@ -7,6 +7,25 @@ class Student{
         return "Roll No: "+rollNo+" Name: "+name;
     }
 }
+class Java{
+    void display(){
+        System.out.println("Iam java");
+    }
+}
+class JavaScript{
+    void display(){
+        System.out.println("Iam javascript");
+    }
+}
+
+class programmingLanguage extends Java{
+    Java java = new Java();
+    JavaScript javascript = new JavaScript();
+    void display(){
+        java.display();
+        javascript.display();
+    }
+}
 public class ConceptOFClass {
     public static void main(String[] args) {
         Student obj = new Student();
