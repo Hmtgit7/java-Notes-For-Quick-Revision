46354c54bdd0563e5b79dbdec002b9b8b27391f6
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-02-23 20:07:30 +0530

    stack

--- Stack/Main.java
+++ Stack/Main.java
@@ -2,7 +2,7 @@ package Stack;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Stack stack = new DynamicStack(5);
+        CustomStack stack = new DynamicStack(5);
         stack.push(1);
         stack.push(2);
         stack.push(3);
